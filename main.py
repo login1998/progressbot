897c24147868b994b8b547487669ea690b05a998
login1998 <docomo.ne.jp1998@gmail.com>
2018-02-13 17:17:30 +0900

    Add stair

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-a, b = 0, 1
-while b < 10:
-    print(b)
-    a, b = b, a+b
+def stair(n):
+    for i in range(0,n+1):
+        num=""
+        for j in range(0,i+1):
+            num+=str(j)
+        print(num)
+
+def pi(n):
+    sharp=""
+    for i in range(0,n+1):
+        sharp=sharp +"#"
+        print(sharp)
+    while(n>0):
+        print(sharp[0:n])
+        n-=1
+
+x= int(input("Please enter an integer"))
+
+for i in range(0,x):
+    pi(i)
